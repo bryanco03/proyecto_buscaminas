b7885d734cf3c1c5d0e44f518ad26424cdf4d898
bryanco03 <bryanco0310@gmail.com>
2023-12-05 15:00:19 -0600

    mostrar el tiempo durado en la partida

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from game import Game
 from tablero import Tablero
 
-game =  Game()
-game.run()
 """
 Crea una instancia de la clase Game
 
@@ -10,3 +8,9 @@ Con el método run() inicia el juego de buscaminas.
 
 Muestra el menú principal y maneja los eventos del juego.
 """
+def main():
+    game =  Game() 
+    game.run()
+
+if __name__ == "__main__":
+    main()
